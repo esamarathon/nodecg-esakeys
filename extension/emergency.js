405e0f72d7999c378da9b29d3c6f81bc3acf368a
zoton2 <zoton2@gmail.com>
2019-07-16 07:33:01 +0100

    Built files

---
+++ extension/emergency.js
@@ -0,0 +1,54 @@
+"use strict";
+const nodecg = require('./utils/nodecg-api-context').get();
+const xkeys = require('./utils/xkeys');
+const obs = nodecg.extensions['esa-layouts'].obs;
+const emergencyMode = nodecg.Replicant('emergencyMode');
+var emergencyDoubleCheck = false;
+var emergencyTimeout;
+if (!Object.entries(xkeys).length)
+    return;
+// Listen to pressed keys.
+xkeys.on('downKey', keyIndex => {
+    // Disable everything if emergency mode is on.
+    if (emergencyMode.value)
+        return;
+    // Keys are sent as strings.
+    keyIndex = parseInt(keyIndex);
+    // Emergency button.
+    // Big key at the bottom left is both 7 and 15; only need to use one.
+    // Emergency mode is currently permanant, maybe need a way to clear the setting?
+    if (keyIndex === 7) {
+        // We don't want the button to be active straight away, so it needs to be pressed twice.
+        if (!emergencyDoubleCheck) {
+            emergencyDoubleCheck = true;
+            // Make the lights blink red.
+            xkeys.setBacklight(7, true, true, true);
+            xkeys.setBacklight(15, true, true, true);
+            // Set timeout so we stop waiting after 10 seconds.
+            emergencyTimeout = setTimeout(() => {
+                emergencyDoubleCheck = false;
+                xkeys.setBacklight(7, false, true);
+                xkeys.setBacklight(15, false, true);
+            }, 10000);
+        }
+        else {
+            nodecg.log.warn('EMERGENCY MODE ACTIVATED, SWITCHING SCENE AND MUTING AUDIO!');
+            emergencyMode.value = true;
+            clearTimeout(emergencyTimeout);
+            // Make the lights solid red.
+            xkeys.setBacklight(7, true, true);
+            xkeys.setBacklight(15, true, true);
+            // Mute the audio source set in the config.
+            var muteOptions = { source: nodecg.bundleConfig.emergencyMode.audioSource, mute: true };
+            obs.send('SetMute', muteOptions).catch((err) => {
+                nodecg.log.warn(`Cannot mute OBS source [${muteOptions.source}]: ${err.error}`);
+            });
+            // Switch to the scene set in the config.
+            var sceneOptions = { 'scene-name': nodecg.bundleConfig.emergencyMode.scene };
+            obs.send('SetCurrentScene', sceneOptions).catch((err) => {
+                nodecg.log.warn(`Cannot switch OBS scene [${sceneOptions['scene-name']}]: ${err.error}`);
+            });
+        }
+    }
+});
+//# sourceMappingURL=emergency.js.map
